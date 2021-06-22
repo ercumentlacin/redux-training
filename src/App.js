d633c2f971e90f9ed527b30e48d157ed0c2d7241
Ercüment Laçın <ercument.lacin@akinon.com>
2021-06-23 00:00:26 +0300

    eslint and prettier configure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { Provider } from 'react-redux';
 import store from './redux/store';
-
 import CakeContainer from './components/CakeContainer';
 import './App.css';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className='App'>
+      <div className="App">
         <CakeContainer />
       </div>
     </Provider>
