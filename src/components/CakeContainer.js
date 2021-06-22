f53726335c950fb3424baf8618c5a02d9e8e2324
Ercüment Laçın <ercument.lacin@akinon.com>
2021-06-22 09:48:00 +0300

    Add CakeContainer Components

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { buyCake } from '../redux';
+
+function CakeContainer(props) {
+  const { numOfCakes, buyCake } = props;
+
+  return (
+    <div>
+      <h2>Number of cakes - {numOfCakes} </h2>
+      <button onClick={buyCake}>Buy Cake</button>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
